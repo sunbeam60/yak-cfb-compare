cfeb9d5c1006c9abf546b27be19dc517c557d73b
Bjorn Toft Madsen <bjorn@toftmadsen.org>
2026-02-27 10:28:50 +0000

    updated with Yak 0.11.2, reran numbers on machine S1

--- yak-upstream/yak_pytest/conftest.py
+++
@@ -1,7 +0,0 @@
-def pytest_addoption(parser):
-    parser.addoption(
-        "--burn-seconds",
-        type=float,
-        default=10.0,
-        help="How long thread-safety tests should burn (default: 10s)",
-    )
